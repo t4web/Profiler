2c7c681f1c0653122b1fcf6a24ef8a8a21fccec3
maxgu <gulturyan@t4web.com.ua>
2016-07-27 09:28:48 +0300

    draft Module.php

--- src/Module.php
+++ src/Module.php
@@ -2,8 +2,42 @@
 
 namespace T4web\Profiler;
 
+use Zend\ModuleManager\ModuleManager;
+use Zend\EventManager\EventManager;
+use Zend\EventManager\EventInterface;
+use Zend\Http\PhpEnvironment\Request;
+use Zend\ModuleManager\Exception\MissingDependencyModuleException;
+use Zend\Mvc\MvcEvent;
+
 class Module
 {
+    public function onBootstrap(EventInterface $e)
+    {
+        /** @var ModuleManager $moduleManager */
+        $moduleManager = $e->getApplication()
+            ->getServiceManager()
+            ->get('modulemanager');
+
+        if (!$moduleManager->getModule('T4web\\DefaultService')) {
+            throw new MissingDependencyModuleException('Module "T4web\\DefaultService" must be enabled in your
+                config/application.config.php. For details see https://github.com/t4web/Profiler#post-installation.');
+        }
+
+        /** @var EventManager $eventManager */
+        $eventManager = $e->getApplication()->getEventManager();
+
+        $eventManager->attach(MvcEvent::EVENT_ROUTE, function(EventInterface $e) {
+            /** @var Request $request */
+            $request = $e->getRequest();
+
+            if (! $request instanceof Request) {
+                return;
+            }
+
+            die(var_dump(__METHOD__));
+        }, -1000);
+    }
+
     public function getAutoloaderConfig()
     {
         return array(
